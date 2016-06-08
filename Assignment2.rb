802948739c8725106ac0dcd0fe0c988ae111d623
joecaintic <joecaintic@yahoo.com>
2016-06-08 13:12:08 -0400

    initial work for assignment 2

---
+++ Assignment2.rb
@@ -0,0 +1,17 @@
+class MyStack
+    
+    example_array = []
+    
+    def push(element)
+        example_array.length + 1 = example_array.length
+        example_array[example_array.length - 1] = element
+        example_array
+    end
+    
+    def pop
+        example_array[example_array.length - 1] = nil
+        example_array.length - 1 = example_array.length
+        example_array
+    end
+
+end
