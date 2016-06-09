374a24f26ac91cb18d9b67439ef59bb5aa58c956
joecaintic <joecaintic@yahoo.com>
2016-06-09 14:35:32 -0400

    more work

--- Assignment2.rb
+++ Assignment2.rb
@@ -1,17 +1,49 @@
 class MyStack
     
-    example_array = []
+    def initialize
+        self.stack_array = []
+    end
     
     def push(element)
-        example_array.length + 1 = example_array.length
-        example_array[example_array.length - 1] = element
-        example_array
+        self.stack_array.length + 1 = self.stack_array.length
+        self.stack_array[self.stack_array.length - 1] = element
     end
     
     def pop
-        example_array[example_array.length - 1] = nil
-        example_array.length - 1 = example_array.length
-        example_array
+        self.stack_array[self.stack_array.length - 1] = nil
+        self.stack_array.length - 1 = self.stack_array.length
+        self.stack_array
+    end
+    
+    def empty?
+        if self.stack_array = []
+            return true
+        else
+            return false
+        end
+    end
+
+end
+
+
+class MyQueue
+
+    DEF ENQUEUE(element)
+        Add element to the end of the list
+    END DEF
+
+    DEF DEQUEUE
+        SET temp to the first element
+        Left-Shift the list by one entry
+        RETURN temp
+    END DEF
+
+    def empty?
+        if self.stack_array = []
+            return true
+        else
+            return false
+        end
     end
 
 end
